dd735217ff55a445e55e45fb11248b2c1c3b169c
noerios <noerios@gmail.com>
2021-02-17 20:35:29 -0600

    server work

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const Workout = require("./userModel")
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//add api routes
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
