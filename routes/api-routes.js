bd0548c82fee2cb057428716b568cadbaea22793
noerios <noerios@gmail.com>
2021-02-23 20:47:31 -0600

    api route from class

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,14 @@
 const router = require("express").Router();
 const db = require("../models");
+
+
+//update below
+router.post("/api/transaction", ({ body }, res) => {
+    Transaction.create(body)
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
