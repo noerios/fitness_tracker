50798f3057a09cf526b4b3e402a4e4c84394298c
noerios <noerios@gmail.com>
2021-02-27 09:58:09 -0600

    git ignore

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 
 const PORT = process.env.PORT || 3000;
 
-const Workout = require("./userModel")
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
