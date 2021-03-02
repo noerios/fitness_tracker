2aaec14021dfb870d803b0e744d11b7b376faad1
noerios <noerios@gmail.com>
2021-03-01 18:13:52 -0600

    more api work

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,3 +12,19 @@ router.post("/api/workouts", ({ body }, res) => {
         res.status(400).json(err);
       });
   });
+
+  router.get("/api/workouts", (req, res) => {
+      db.Workout.aggregate([{
+          $addFields: {
+              totalDuration: {
+                  $sum: '$exercises.duration',
+              },
+          },
+      }])
+      .then((dbWorkouts) => {
+          res.json(dbWorkouts);
+      })
+      .catch((err) => {
+          res.json(err);
+      });
+  });
